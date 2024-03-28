553b5023dfd0dfd0d004766ba9769172f04e625c
calves20 <157167670+calves20@users.noreply.github.com>
2024-03-27 23:13:39 -0700

    Update ts_hashmap.h

--- ts_hashmap.h
+++ ts_hashmap.h
@@ -1,27 +1,24 @@
+#ifndef TS_HASHMAP_H
+#define TS_HASHMAP_H
+// Author @Curtis Alves
 #include <pthread.h>
+#define HASH_MAP_SIZE 1024
 
-// A hashmap entry stores the key, value
-// and a pointer to the next entry
-typedef struct ts_entry_t {
-   int key;
-   int value;
-   struct ts_entry_t *next;
-} ts_entry_t;
+typedef struct {
+    int key;
+    int value;
+} HashItem;
 
-// A hashmap contains an array of pointers to entries,
-// the capacity of the array, the size (number of entries stored), 
-// and the number of operations that it has run.
-typedef struct ts_hashmap_t {
-   ts_entry_t **table;
-   int numOps;
-   int capacity;
-   int size;
-} ts_hashmap_t;
+typedef struct {
+    HashItem** items;
+    pthread_mutex_t mutex;
+    int numOps; // Tracks the number of operations performed
+} HashMap;
 
-// function declarations
-ts_hashmap_t *initmap(int);
-int get(ts_hashmap_t*, int);
-int put(ts_hashmap_t*, int, int);
-int del(ts_hashmap_t*, int);
-void printmap(ts_hashmap_t*);
-void freeMap(ts_hashmap_t*);
+HashMap *initmap(int capacity);
+void freeMap(HashMap *map);
+int put(HashMap *map, int key, int value);
+int get(HashMap *map, int key);
+int del(HashMap *map, int key);
+
+#endif
